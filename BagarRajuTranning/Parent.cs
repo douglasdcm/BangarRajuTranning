d37f470b5a01bea6bb8b43784a5ba5783c18b9fc
Douglas Cardoso Rezende Morais <dmorais@mmsi.com>
2020-03-03 12:46:58 -0300

    adding files

---
+++ BagarRajuTranning/Parent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagarRajuTranning
+{
+    class ParentClass
+    {
+        public virtual void Test1()
+        {
+            Console.WriteLine("Method T1 from parent class");
+        }
+
+        public void Test2()
+        {
+            Console.WriteLine("Method T2 from parent class");
+        }
+
+    }
+
+    class ChildClass : ParentClass
+    {
+        //reimplement the parent method (virtual method)
+        //override is allowed just for virtual classes
+        public override void Test1()
+        {
+            Console.WriteLine("Method T1 from child class");
+        }
+
+        //method hidding/shadowing (using the new)
+        //the parent class can not call it because it is hidden
+        public new void Test2() 
+        {
+            Console.WriteLine("Method T1 from child class");
+        }
+
+        public void ParenteTest1()
+        {
+            base.Test1();
+        }
+
+        public void ParenteTest2()
+        {
+            base.Test2();
+        }
+
+    }
+
+}
--- BagarRajuTranning/Program.cs
+++ BagarRajuTranning/Program.cs
@@ -42,11 +42,25 @@ namespace BagarRajuTranning
 
             //Access specifier demo
 
-            //types of variables
-            const int c = 2; //must be initialided and never changes. it is the same for all instances
-            Console.WriteLine(c);
-            Program p = new Program();
-            Console.WriteLine(p.r);
+            ////types of variables
+            //const int c = 2; //must be initialided and never changes. it is the same for all instances
+            //Console.WriteLine(c);
+            //Program p = new Program();
+            //Console.WriteLine(p.r);
+
+            //method hiding
+            //reimplementing a parent class method under the child class with the same signature
+            ChildClass cc = new ChildClass();
+            cc.Test1();
+            cc.Test2();
+            cc.ParenteTest1(); //point to parent method
+            cc.ParenteTest2();
+
+            ParentClass pc = cc; //point a parent class to a child class
+            pc.Test1(); //can call overrided classes in the child class
+                        //this is an approach to call a child method from the parent class
+            pc.Test2(); //can call only the parent methods
+
 
             Console.ReadLine();
         }
